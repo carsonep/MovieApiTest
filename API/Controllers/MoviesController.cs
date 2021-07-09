c2d11acd65e07651e9f24bcf165675f18649331d
carsonep <carsonepeterson@gmail.com>
2021-07-09 16:56:23 -0500

    Refactor Movies Controller

--- API/Controllers/MoviesController.cs
+++ API/Controllers/MoviesController.cs
@@ -18,6 +18,36 @@ namespace API.Controllers
             _dbContext = dbContext;
         }
 
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult AllMovies()
+        {
+            var movies = from movie in _dbContext.Movies
+                         select new
+                         {
+                            Id = movie.Id,
+                            Name = movie.Name,
+                            Duration = movie.Duration,
+                            Language = movie.Language,
+                            Rating = movie.Rating,
+                            Genre = movie.Genre,
+                            ImageUrl = movie.ImageUrl
+                         };
+            return Ok(movies);
+        }
+
+        [Authorize]
+        [HttpGet("[action]/{id}")]
+        public IActionResult MovieDetail(int id)
+        {
+           var movie = _dbContext.Movies.Find(id);
+           if (movie == null)
+           {
+               return NotFound();
+           }
+           return Ok(movie);
+        }
+
         [Authorize(Roles = "Admin")]
         [HttpPost]
         public IActionResult Post([FromForm] Movie movieObj)
@@ -33,6 +63,57 @@ namespace API.Controllers
             _dbContext.Movies.Add(movieObj);
             _dbContext.SaveChanges();
             return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromForm] Movie movieObj)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound("No Record found against this Id");
+            }
+            else {
+                var guid = Guid.NewGuid();
+                var filePath = Path.Combine("wwwroot",guid + ".jpg");
+                if(movieObj.Image != null)
+                {
+                    var fileStream = new FileStream(filePath, FileMode.CreateNew);
+                    movieObj.Image.CopyTo(fileStream);
+                    movie.ImageUrl = filePath.Remove(0,7);
+                }
+
+                movie.Name = movieObj.Name;
+                movie.Description = movieObj.Description;
+                movie.Language = movieObj.Language;
+                movie.Duration = movieObj.Duration;
+                movie.PlayingDate = movieObj.PlayingDate;
+                movie.PlayingTime = movieObj.PlayingTime;
+                movie.Rating = movieObj.Rating;
+                movie.Genre = movieObj.Genre;
+                movie.TrailorUrl = movieObj.TrailorUrl;
+                movie.TicketPrice = movieObj.TicketPrice;
+
+                _dbContext.SaveChanges();
+                return Ok("Record Updated Successfully");
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+           var movie = _dbContext.Movies.Find(id);
+           if (movie == null)
+            {
+                return NotFound("No Record found against this Id");
+            } else {
+                _dbContext.Movies.Remove(movie);
+                _dbContext.SaveChanges();
+                return Ok("Record Deleted Successfully");
+            }
+            
         }
     }
 }
